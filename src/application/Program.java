d2ce293fc4c3e3d22e0b0d7262f23f4b021fdbeb
warriorsyn <mjcgamer32@gmail.com>
2018-12-31 02:18:38 -0300

    Java DAO exemple Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dp = new Department(1, "books");
-		System.out.println(dp);
+		
+		Seller seller = new Seller(12, "Joao", "joao@gmail.com", new Date(), 3000.00, dp);
+		System.out.println(seller);
 		
 	}
 
